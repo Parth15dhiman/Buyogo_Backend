ecfd376f9f87663521634ca263beec58f7a2207f
Parth _ <3315.parth@gmail.com>
2026-01-28 18:34:47 +0530

    first commit

---
+++ src/main/java/com/factory/buyogobackend/BuyogobackendApplication.java
@@ -0,0 +1,13 @@
+package com.factory.buyogobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyogobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyogobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/buyogobackend/BuyogobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.buyogobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyogobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
